ffcb951bec1bf43aab7d3d627a1887310bcc8d34
JasonLong24 <jasonlongball@gmail.com>
2019-02-09 17:47:28 -0500

    pid wrist

---
+++ src/main/java/frc/robot/commands/Arm/PIDWrist.java
@@ -0,0 +1,42 @@
+package frc.robot.commands.Arm;
+
+import frc.robot.RobotMap;
+import frc.robot.commands.CommandBase;
+
+import edu.wpi.first.wpilibj.command.PIDCommand;
+
+public class PIDWrist extends PIDCommand{
+
+	public PIDWrist(double angle) {
+		super(RobotMap.wristP, RobotMap.wristI, RobotMap.wristD);
+
+		setTimeout(3000);
+		setSetpoint(angle);
+	}
+
+	protected void initialize() {
+	}
+
+	@Override
+	protected double returnPIDInput() {
+		return CommandBase.intake.getWristAngle();
+	}
+
+	@Override
+	protected void usePIDOutput(double speed) {
+        CommandBase.intake.moveWrist(speed);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return isTimedOut() || Math.abs(getSetpoint() - getPosition()) < .5;
+	}
+
+	protected void end() {
+		setTimeout(0);
+	}
+
+	protected void interrupted() {
+		end();
+	}
+}
